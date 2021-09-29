0ae659f547b956ecba38875535161b91de582449
DiegoEGarciaG <daigogar@gmail.com>
2021-09-28 23:23:10 -0300

    Se realiza el desafio semanal completo

--- cap4/emergency_stop.py
+++ cap4/emergency_stop.py
@@ -3,7 +3,7 @@
 """
 Este programa implementa un freno de emergencia para evitar accidentes en Duckietown.
 """
-
+import math
 import sys
 import argparse
 import gym
@@ -27,15 +27,23 @@ def mov_duckiebot(key):
     return action
 
 def det_duckie(obs):
-    ### DETECTOR HECHO EN LA MISIÓN ANTERIOR
     dets = list()
-
+    lower_yellow = np.array([21, 100, 100])
+    upper_yellow = np.array([31, 255, 255])
+    min_area = 1750
+    image_out = cv2.cvtColor(obs, cv2.COLOR_RGB2HSV)
+    mask = cv2.inRange(image_out, lower_yellow, upper_yellow)
+    image_out = cv2.bitwise_and(image_out, image_out, mask= mask)
+    kernel = np.ones((5,5),np.uint8)
+    image_out = cv2.erode(image_out, kernel, iterations = 2)
+    image_out = cv2.dilate(image_out, kernel, iterations = 2)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-
+        x,y,w,h = cv2.boundingRect(cnt)
+        AREA = w*h
         if AREA > min_area:
             # En lugar de dibujar, se agrega a la lista
             dets.append((x,y,w,h))
-
     return dets
 
 def draw_dets(obs, dets):
@@ -44,14 +52,12 @@ def draw_dets(obs, dets):
         x2 = x1 + d[2]
         y2 = y1 + d[3]
         cv2.rectangle(obs, (int(x1), int(y1)), (int(x2),int(y2)), (0,255,0), 3)
-
     return obs
 
 def red_alert(obs):
-    red_img = np.zeros((480, 640, 3), dtype = np.uint8)
+    red_img = np.zeros(obs.shape, dtype = np.uint8)
     red_img[:,:,0] = 255
     blend = cv2.addWeighted(obs, 0.5, red_img, 0.5, 0)
-
     return blend
 
 if __name__ == '__main__':
@@ -81,7 +87,7 @@ if __name__ == '__main__':
     duck_pos = np.array([2,0,2])
 
     # Constante que se debe calcular
-    C = 1 # f * dr (f es constante, dr es conocido)
+    C = 1 #(f es constante, dr es conocido)
 
     while True:
 
@@ -96,30 +102,30 @@ if __name__ == '__main__':
 
         # Si hay alerta evitar que el Duckiebot avance
         if alert:
-            pass
+            action[0] = -1.0
 
         # Se ejecuta la acción definida anteriormente y se retorna la observación (obs),
         # la evaluación (reward), etc
         obs, reward, done, info = env.step(action)
 
         # Detección de patos, retorna lista de detecciones
-
+        dets = det_duckie(obs)
         # Dibuja las detecciones
-
+        draw_dets(obs,dets)
         # Obtener posición del duckiebot
         dbot_pos = env.cur_pos
         # Calcular distancia real entre posición del duckiebot y pato
         # esta distancia se utiliza para calcular la constante
-        dist = CALCULAR
-
+        dist = (dbot_pos - duck_pos)
+        dist = (dist[0]**2 + dist[1]**2 + dist[2]**2)**(1/2)
         # La alerta se desactiva (opción por defecto)
         alert = False
         
         for d in dets:
             # Alto de la detección en pixeles
-            p = DEFINIR
+            p = d[3]
             # La aproximación se calcula según la fórmula mostrada en la capacitación
-            d_aprox = DEFINIR
+            d_aprox = (0.08*717.283857)/(p)
 
             # Muestra información relevante
             print('p:', p)
@@ -127,12 +133,13 @@ if __name__ == '__main__':
             print('Dr:', dist)
 
             # Si la distancia es muy pequeña activa alerta
+            # Si fuera presencial se debería ver la distancia de frenado en función de velocidad no?
             if d_aprox < 0.3:
                 # Activar alarma
-
+                alert = True
                 # Muestra ventana en rojo
-
-        # Se muestra en una ventana llamada "patos" la observación del simulador
+                obs = red_alert(obs)
+            # Se muestra en una ventana llamada "patos" la observación del simulador
         cv2.imshow('patos', cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
 
     # Se cierra el environment y termina el programa
